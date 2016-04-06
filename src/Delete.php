6cc3a09936e5205e4b00b08e3bee64b4db18cac5
DjLeChuck <djlechuck@gmail.com>
2016-04-06 23:05:24 +0200

    add DELETE queries with some tests

---
+++ src/Delete.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace YAQB;
+
+/**
+ * Select builder.
+ */
+class Delete extends AbstractBuilder
+{
+    /**
+     * Renders the whole query.
+     *
+     * @return string
+     */
+    public function render()
+    {
+        return implode(' ', array_filter([
+            'DELETE',
+            $this->renderFrom(),
+            $this->renderJoin(),
+            $this->renderWhere(),
+            $this->renderGroupBy(),
+            $this->renderOrderBy(),
+            $this->renderLimit(),
+        ]));
+    }
+}
---
+++ tests/src/DeleteTest.php
@@ -0,0 +1,65 @@
+<?php
+
+use YAQB\AbstractBuilder;
+use YAQB\Expressions\Literal;
+use YAQB\Delete;
+
+class DeleteTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Test a simple DELETE query.
+     */
+    public function testSimpleDelete()
+    {
+        $expected   = 'DELETE FROM `users`';
+        $builder    = (new Delete())
+            ->from('users')
+        ;
+
+        $this->assertEquals($expected, $builder->render());
+
+        // Add some WHERE clauses
+        $expected .= ' WHERE `email` LIKE \'%@domain.tld\' AND `disabled` != 1 AND `date_last_connexion` > DATE_SUB(NOW(), INTERVAL 3 DAYS)';
+
+        $builder
+            ->whereLike('email', '%@domain.tld')
+            ->whereNot('disabled', true)
+            ->whereGt('date_last_connexion', new Literal('DATE_SUB(NOW(), INTERVAL 3 DAYS)'))
+        ;
+
+        $this->assertEquals($expected, $builder->render());
+
+        // Add ORDER BY and LIMIT
+        $expected .= ' ORDER BY `lastname`, `firstname` LIMIT 10';
+
+        $builder
+            ->orderBy('lastname')
+            ->orderBy('firstname')
+            // Another possible writing
+            //->orderBy(['lastname', 'firstname'])
+            ->limit(10)
+        ;
+
+        $this->assertEquals($expected, $builder->render());
+    }
+
+    /**
+     * Test an intermediary DELETE query.
+     */
+    public function testIntermediaryDelete()
+    {
+        $expected = 'DELETE FROM `users` '
+            . 'INNER JOIN `civilitytitles` `c` ON `id_civility` = `c`.`id` LEFT JOIN `orders` ON `users`.`id` = `orders`.`id_user` '
+            . 'WHERE `orders`.`id` != NULL ORDER BY MAX(`total_tax_inclusive`)';
+
+        $builder = (new Delete())
+            ->from('users')
+            ->innerJoin(['c' => 'civilitytitles'], [['id_civility', ['c' => 'id']]])
+            ->leftJoin('orders', [[['users' => 'id'], ['orders' => 'id_user']]])
+            ->whereNot(['orders' => 'id'], null)
+            ->orderBy(new Literal(sprintf('MAX(%s)', AbstractBuilder::quote('total_tax_inclusive'))))
+        ;
+
+        $this->assertEquals($expected, $builder->render());
+    }
+}
