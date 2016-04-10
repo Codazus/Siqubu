93d1ca368bb9624700bd1dbfb8c271513d491715
DjLeChuck <djlechuck@gmail.com>
2016-04-11 00:17:25 +0200

    Modify AbstractBuilder
    - Remove useless methods about escape, quote, DB
    - Add named parameters

--- src/AbstractBuilder.php
+++ src/AbstractBuilder.php
@@ -3,10 +3,7 @@
 namespace Siqubu;
 
 use InvalidArgumentException;
-use mysqli;
-use PDO;
 use Siqubu\Expressions\CloseBracket;
-use Siqubu\Expressions\Literal;
 use Siqubu\Expressions\OpenBracket;
 use Siqubu\Expressions\OrOperator;
 use SplQueue;
@@ -17,11 +14,6 @@ use SplQueue;
 abstract class AbstractBuilder
 {
     /**
-     * DEFAULT ESCAPE IDENTIFIER
-     */
-    const DEFAULT_ESCAPE_IDENTIFIER = "'";
-
-    /**
      * The current SplQueue where to push ExpressionsInterface.
      *
      * @var SplQueue
@@ -29,11 +21,43 @@ abstract class AbstractBuilder
     protected $current_expression_queue;
 
     /**
-     * An instance of DB.
+     * Query parameters
      *
-     * @var mixed
+     * @var array
+     *
+     * @return AbstractBuilder
      */
-    protected static $db;
+    protected $parameters = [];
+
+    /**
+     * Defines query parameters.
+     *
+     * @param array $parameters Query parameters
+     *
+     * @return AbstractBuilder
+     */
+    public function setParameters(array $parameters)
+    {
+        foreach ($parameters as $key => $value) {
+            if (is_numeric($key)) {
+                throw new InvalidArgumentException('Each parameters must have a nominative key.');
+            }
+        }
+
+        $this->parameters += $parameters;
+
+        return $this;
+    }
+
+    /**
+     * Returns query parameters.
+     *
+     * @return array
+     */
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
 
     /**
      * Open a bracket.
@@ -85,94 +109,4 @@ abstract class AbstractBuilder
      * @return string
      */
     abstract protected function render();
-
-    /**
-     * Returns the alias information.
-     *
-     * @param mixed $value Value or associative array with alias and value
-     *
-     * @return array
-     */
-    protected function getAliasData($value)
-    {
-        if (is_array($value)) {
-            return [
-                key($value),
-                current($value),
-            ];
-        }
-
-        return [
-            null,
-            $value,
-        ];
-    }
-
-    /**
-     * Quote the value.
-     *
-     * @param string $value Value to quote
-     *
-     * @return string
-     */
-    public static function quote($value)
-    {
-        if (empty($value)) {
-            return null;
-        }
-
-        return static::$quote_identifier.$value.static::$quote_identifier;
-    }
-
-    /**
-     * Escapes the value. Uses the DB capability if provided.
-     *
-     * @param string $value Value to quote
-     *
-     * @return string
-     */
-    public static function escape($value)
-    {
-        if (static::$db instanceof PDO) {
-            return static::$db->quote($value);
-        } elseif (static::$db instanceof mysqli) {
-            return sprintf('\'%s\'', static::$db->real_escape_string($value));
-        }
-
-        if (null === $value) {
-            return 'NULL';
-        } elseif (is_bool($value)) {
-            return (int) $value;
-        } elseif ($value instanceof Literal) {
-            return $value->render();
-        } elseif ($value instanceof Select) {
-            return sprintf('(%s)', $value);
-        }
-
-        return static::DEFAULT_ESCAPE_IDENTIFIER.str_replace(static::DEFAULT_ESCAPE_IDENTIFIER, sprintf('\\%s', static::DEFAULT_ESCAPE_IDENTIFIER), $value).static::DEFAULT_ESCAPE_IDENTIFIER;
-    }
-
-    /**
-     * Set the quote identifier used by the builder.
-     *
-     * @param string $identifier Identifier to use
-     */
-    public static function setQuoteIdentifier($identifier)
-    {
-        static::$quote_identifier = $identifier;
-    }
-
-    /**
-     * Set the instance of the DB used by the builder to escape values.
-     *
-     * @param string $db Identifier to use
-     */
-    public static function setDb($db)
-    {
-        if (!($db instanceof PDO || $db instanceof mysqli)) {
-            throw new InvalidArgumentException('The DB must be an instance of \PDO or \mysqli.');
-        }
-
-        static::$db = $db;
-    }
 }
